3c7f9c633e8426eddf7dabe63e2e02c4cda1128c
mango606 <yuj08184@kangwon.ac.kr>
2025-04-05 14:43:46 +0900

    fix: 주문 서비스 Kafka 설정 클래스 수정
    - 이벤트 타입별로 전용 컨테이너 팩토리를 생성하고
    setErrorHandler 대신 CommonErrorHandler 사용
    - 메시지 역직렬화 설정을 지정하여 이벤트 수신 문제 해결
    
    Related to: #59

--- order/src/main/java/com/_hateam/order/infrastructure/config/KafkaConfig.java
+++ order/src/main/java/com/_hateam/order/infrastructure/config/KafkaConfig.java
@@ -1,14 +1,21 @@
 package com._hateam.order.infrastructure.config;
 
+import com._hateam.common.event.DeliveryCreatedEvent;
+import com._hateam.common.event.DeliveryStatusChangedEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,5 +38,98 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, Object.class.getName());
+        props.put(JsonDeserializer.TYPE_MAPPINGS,
+                "delivery-created:com._hateam.common.event.DeliveryCreatedEvent," +
+                        "delivery-status-changed:com._hateam.common.event.DeliveryStatusChangedEvent");
+
+        return new DefaultKafkaConsumerFactory<>(props,
+                new StringDeserializer(),
+                new JsonDeserializer<>(Object.class, false));
+    }
+
+    @Bean
+    public CommonErrorHandler errorHandler() {
+        // 에러 발생 시 최대 3번, 5초 간격으로 재시도
+        return new DefaultErrorHandler(
+                (record, exception) -> {
+                    System.err.println("Kafka 메시지 처리 중 오류 발생: " + exception.getMessage());
+                    exception.printStackTrace();
+                },
+                new FixedBackOff(5000L, 3L)
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(errorHandler());
+        return factory;
+    }
+
+    // DeliveryCreatedEvent 전용 컨테이너 팩토리
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, DeliveryCreatedEvent> deliveryCreatedListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, DeliveryCreatedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+
+        ConsumerFactory<String, DeliveryCreatedEvent> specificConsumerFactory =
+                new DefaultKafkaConsumerFactory<>(
+                        props,
+                        new StringDeserializer(),
+                        new JsonDeserializer<>(DeliveryCreatedEvent.class, false));
+
+        factory.setConsumerFactory(specificConsumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
+        return factory;
+    }
+
+    // DeliveryStatusChangedEvent 전용 컨테이너 팩토리
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, DeliveryStatusChangedEvent> statusChangedListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, DeliveryStatusChangedEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+
+        ConsumerFactory<String, DeliveryStatusChangedEvent> specificConsumerFactory =
+                new DefaultKafkaConsumerFactory<>(
+                        props,
+                        new StringDeserializer(),
+                        new JsonDeserializer<>(DeliveryStatusChangedEvent.class, false));
+
+        factory.setConsumerFactory(specificConsumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
+        return factory;
     }
 }
